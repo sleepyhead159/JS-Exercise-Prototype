20814481f09dd9d33eac2d15812269d7a1d45048
sleepyhead159 <daniel.abbott.w@gmail.com>
2019-12-19 14:41:51 -0700

    Finished project

--- index.js
+++ index.js
@@ -39,8 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+      return this.stomach.push(someFood)
+  }
+}
+
+Person.prototype.poop = function() {
+  return this.stomach = []
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}${this.age}`
 }
 
 /*
@@ -57,9 +73,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  return this.tank += gallons;
+};
 
 /*
   TASK 3
@@ -68,18 +91,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window binding - Using strict, no other rules apply, prevents the window binding from breaking the code.
+  2. Implicit Binding - looking to the left of the dot referring to what this actually means.
+  3. Explicit Binding - invokes the functions either one by one, array.
+  4. New Binding - constructing a new object and "this" refers too.
 */
 
 
